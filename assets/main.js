0be9cca93b24f5e333c926d83c81556ebb92ac95
Shajid <shajid1biz@gmail.com>
2023-12-11 16:20:27 +0530

    started the dom

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const depositEl = document.querySelector("#depositInput");
+const debitedEl = document.querySelector("#debitedInput");
+const intraEl = document.querySelector("#intraInput");
+
+const depositBtn = depositEl.nextElementSibling;
+
+// Should add numbers only without any texts.
+depositBtn.addEventListener("click", () => {
+  console.log(depositEl.value);
+});
+
+// Then It should equal with the card # 1 which is deposited.
