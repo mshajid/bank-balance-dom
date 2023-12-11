b206b7e27a71a28c58e0d246c0de443e03cf41dc
Shajid <shajid1biz@gmail.com>
2023-12-11 17:07:29 +0530

    deposited value completed

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,18 @@
 const depositEl = document.querySelector("#depositInput");
 const debitedEl = document.querySelector("#debitedInput");
 const intraEl = document.querySelector("#intraInput");
-
 const depositBtn = depositEl.nextElementSibling;
+
+const depositAmountEl = document.querySelector("#depositAmount");
 
 // Should add numbers only without any texts.
 depositBtn.addEventListener("click", () => {
-  console.log(depositEl.value);
+  const convertDepositEl = parseInt(depositEl.value);
+  const depositAmountConvert = parseInt(depositAmountEl.textContent);
+  const finalDepositedAmount = convertDepositEl + depositAmountConvert;
+  
+  depositAmountEl.innerHTML = finalDepositedAmount
+
 });
 
 // Then It should equal with the card # 1 which is deposited.
